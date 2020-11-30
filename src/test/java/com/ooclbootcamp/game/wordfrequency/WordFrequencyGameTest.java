7e819a8039ac9b1042b5c92077c6ad1082f98bca
Hins Chiu <chiuhi2@oocl.com>
2020-11-30 18:51:51 +0800

    [wordFrequencyRefactor][HinsChiu]refactor:rename test case

--- src/test/java/com/ooclbootcamp/game/wordfrequency/WordFrequencyGameTest.java
+++ src/test/java/com/ooclbootcamp/game/wordfrequency/WordFrequencyGameTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class WordFrequencyGameTest {
 
     @Test
-    public void should_get_the_1_when_input_the() throws Exception {
+    public void should_return_the_1_when_get_result_given_the() throws Exception {
         //Given
         String inputStr = "the";
         String expectResult = "the 1";
@@ -17,7 +17,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words() throws Exception {
+    public void should_return_the_1_is_1_when_get_result_given_the_is() throws Exception {
         //Given
         String inputStr = "the is";
         String expectResult = "the 1\nis 1";
@@ -25,7 +25,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_spaces() throws Exception {
+    public void should_return_the_1_is_1_when_get_result_given_the_is_with_special_spaces() throws Exception {
         //Given
         String inputStr = "the      is";
         String expectResult = "the 1\nis 1";
@@ -33,7 +33,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_two_words_with_special_enter() throws Exception {
+    public void should_return_the_1_is_1_when_get_result_given_the_is_with_special_enter() throws Exception {
         //Given
         String inputStr = "the   \n   is";
         String expectResult = "the 1\nis 1";
@@ -41,7 +41,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_pracess_two_same_words_with_sorted() throws Exception {
+    public void should_return_the_2_is_1_when_get_result_given_the_the_is() throws Exception {
         //Given
         String inputStr = "the the is";
         String expectResult = "the 2\nis 1";
@@ -49,7 +49,7 @@ public class WordFrequencyGameTest {
     }
 
     @Test
-    public void should_process_sorted_with_count_descending() throws Exception {
+    public void should_return_is_2_the_1_when_get_result_given_the_is_is() throws Exception {
         //Given
         String inputStr = "the is is";
         String expectResult = "is 2\nthe 1";
