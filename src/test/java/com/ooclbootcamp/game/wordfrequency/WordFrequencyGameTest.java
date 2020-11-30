9535a147d416186755cacb0f741f75f1ffc8c7ca
Hins Chiu <chiuhi2@oocl.com>
2020-11-30 19:02:20 +0800

    [wordFrequencyRefactor][HinsChiu]refactor:refactor test case with given when then format

--- src/test/java/com/ooclbootcamp/game/wordfrequency/WordFrequencyGameTest.java
+++ src/test/java/com/ooclbootcamp/game/wordfrequency/WordFrequencyGameTest.java
@@ -7,60 +7,77 @@ import com.ooclbootcamp.game.wordfrequency.WordFrequencyGame;
 import org.junit.jupiter.api.Test;
 
 public class WordFrequencyGameTest {
+    private WordFrequencyGame wordFrequencyGame = new WordFrequencyGame();
 
     @Test
-    public void should_return_the_1_when_get_result_given_the() throws Exception {
-        //Given
-        String inputStr = "the";
-        String expectResult = "the 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    void should_return_the_1_when_get_result_given_the() throws CalculateErrorException {
+        //given
+        String sentence = "the";
+
+        //when
+        String result = wordFrequencyGame.getResult(sentence);
+
+        //then
+        assertEquals("the 1", result);
     }
 
     @Test
-    public void should_return_the_1_is_1_when_get_result_given_the_is() throws Exception {
-        //Given
-        String inputStr = "the is";
-        String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    void should_return_the_1_is_1_when_get_result_given_the_is() throws CalculateErrorException {
+        //given
+        String sentence = "the is";
+
+        //when
+        String result = wordFrequencyGame.getResult(sentence);
+
+        //then
+        assertEquals("the 1\nis 1", result);
     }
 
     @Test
-    public void should_return_the_1_is_1_when_get_result_given_the_is_with_special_spaces() throws Exception {
-        //Given
-        String inputStr = "the      is";
-        String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    void should_return_the_1_is_1_when_get_result_given_the_is_with_special_spaces() throws CalculateErrorException {
+        //given
+        String sentence = "the      is";
+
+        //when
+        String result = wordFrequencyGame.getResult(sentence);
+
+        //then
+        assertEquals("the 1\nis 1", result);
     }
 
     @Test
-    public void should_return_the_1_is_1_when_get_result_given_the_is_with_special_enter() throws Exception {
-        //Given
-        String inputStr = "the   \n   is";
-        String expectResult = "the 1\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    void should_return_the_1_is_1_when_get_result_given_the_is_with_special_enter() throws CalculateErrorException {
+        //given
+        String sentence = "the   \n   is";
+
+        //when
+        String result = wordFrequencyGame.getResult(sentence);
+
+        //then
+        assertEquals("the 1\nis 1", result);
     }
 
     @Test
-    public void should_return_the_2_is_1_when_get_result_given_the_the_is() throws Exception {
-        //Given
-        String inputStr = "the the is";
-        String expectResult = "the 2\nis 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    void should_return_the_2_is_1_when_get_result_given_the_the_is() throws CalculateErrorException {
+        //given
+        String sentence = "the the is";
+
+        //when
+        String result = wordFrequencyGame.getResult(sentence);
+
+        //then
+        assertEquals("the 2\nis 1", result);
     }
 
     @Test
-    public void should_return_is_2_the_1_when_get_result_given_the_is_is() throws Exception {
-        //Given
-        String inputStr = "the is is";
-        String expectResult = "is 2\nthe 1";
-        validate_Input_words_process_to_expected_word(inputStr, expectResult);
-    }
+    void should_return_is_2_the_1_when_get_result_given_the_is_is() throws CalculateErrorException {
+        //given
+        String sentence = "the is is";
 
-    private void validate_Input_words_process_to_expected_word(String inputStr, String expectResult) throws CalculateErrorException {
-        WordFrequencyGame game = new WordFrequencyGame();
-        //When
-        String result = game.getResult(inputStr);
-        //Then
-        assertEquals(result, expectResult);
+        //when
+        String result = wordFrequencyGame.getResult(sentence);
+
+        //then
+        assertEquals("is 2\nthe 1", result);
     }
 }
